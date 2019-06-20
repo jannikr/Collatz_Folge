539c6517bc9bdc33f3609d2623a68fed3cbe3370
Jan Nikolai Rückert <31436472+jannikr@users.noreply.github.com>
2019-06-20 22:25:01 +0200

    Create alter.py

---
+++ alter.py
@@ -0,0 +1,11 @@
+import datetime
+now = datetime.datetime.now()
+
+def alter(jahr):
+    if (jahr <= now.year):
+        print("Du bist: " + str(now.year - (jahr+1)) + " Jahre jung.")
+    else:
+        print("Gebe einen Wert kleiner als " + str(now.year) + " an.")
+
+alter(1994) #Du bist 24 Jahre alt. (now.year = 2019)
+alter(2020) #Gebe einen Wert kleiner als (now.year = 2019) an.
