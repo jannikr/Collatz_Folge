f9ff881731cd6afd9b257e1f0b388d1e25f6b638
Jan Nikolai Rückert <31436472+jannikr@users.noreply.github.com>
2019-06-30 11:52:24 +0200

    Update Alter.py

--- Alter.py
+++ Alter.py
@@ -7,5 +7,5 @@ def alter(jahr):
     else:
         print("Gebe einen Wert kleiner als " + str(now.year) + " an.")
 
-alter(1994) #Du bist 24 Jahre alt. (now.year = 2019)
+alter(1994) #Du bist 24 Jahre alt. (now.year = 2019).
 alter(2020) #Gebe einen Wert kleiner als (now.year = 2019) an.
